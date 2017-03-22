c9ce695e0b0a8ef2792c739d3ea6aa9f01642b9e
Jôvani Moura de Oliveira <jovani.moura@outtech.com.br>
2017-03-22 13:41:54 -0300

    commit by outtech on Qua Mar 22 13:35:12 BRT 2017

--- addons_my/library_book/wizard/library_loan_wizard.py
+++ addons_my/library_book/wizard/library_loan_wizard.py
@@ -10,14 +10,39 @@ class LibraryLoanWizard(models.TransientModel):
     member_id = fields.Many2one('library.member', 'Member')
     book_ids = fields.Many2many('library.book', 'Books')
 
+    # refactored in  Wizards and Code reuse p.159 (p.136 in the book).
+    # @api.multi
+    # def record_loans(self):
+    #     for wizard in self:
+    #         member = wizard.member_id
+    #         books = wizard.book_ids
+    #         loan = self.env['library.book.loan']
+    #         for book in books:
+    #             loan.create({
+    #                 'member_id': member.id,
+    #                 'book_id': book.id
+    #             })
+
+    # refactored in  Wizards and Code reuse p.159 (p.136 in the book).
     @api.multi
-    def record_loans(self):
+    def record_borrows(self):
+        # self.ensure_one()
         for wizard in self:
-            member = wizard.member_id
-            books = wizard.book_ids
-            loan = self.env['library.book.loan']
-            for book in books:
-                loan.create({
-                    'member_id': member.id,
-                    'book_id': book.id
-                })
+            member = self.member_id
+            books = self.book_ids
+            member.borrow_books(books)
+        member_ids = self.mapped('member_id').ids
+        action = {
+            'type': 'ir.action.act_window',
+            'name': 'Borrower',
+            'res_model': 'library.member',
+            'domain': [('id', '=', member_ids)],
+            'view_mode': 'form,tree'
+        }
+        return action
+
+
+    def _default_member(self):
+        if self.context.get('active_model' == 'library.member'):
+            return self.self.context.get('active_id', False)
+
