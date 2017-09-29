a4bbc862343fee4e88fcb9c48b66974207806cfd
Jôvani Moura de Oliveira <jovani.moura@outtech.com.br>
2017-09-29 17:30:13 -0300

    commit by outtech on Sex Set 29 17:29:07 BRT 2017

---
+++ addons_odoo_store/customer_vendor_statement/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+# Copyright 2017 Eficent Business and IT Consulting Services S.L.
+#   (http://www.eficent.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'Customer Vendor Statement',
+    'version': '1.0',
+    'category': 'Accounting & Finance',
+    'summary': 'Statement for customer and Vendor by currency',
+    'description':'open any customer or vendor form and click on action--> Customer / Vendor Statement',
+    "author": "Abdallah Mohamed",
+    'website': 'abdalla_mohammed@outlook.com',
+    'depends': [
+        'account',
+    ],
+    'data': [
+        'views/statement.xml',
+        'wizard/customer_vendor_statement_wizard.xml',
+    ],
+    'installable': True,
+    'application': False,
+}
